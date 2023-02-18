845024c151715b76842c1076cb41798a311c30b8
unknown <dipakpaul@gmail.com>
2023-02-18 12:51:22 +0530

    added window confirm cypress specs

--- cypress/e2e/alerts.cy.ts
+++ cypress/e2e/alerts.cy.ts
@@ -1,20 +1,19 @@
 describe('Alerts', () => {
-  it('JS Alerts', () => {
+  it.skip('JS Alerts', () => {
     cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
     cy.get("button[onClick='jsAlert()']").click()
     cy.on('window.alert', (t) => {
-        expect(t).to.contains('msg in the alert window')
+        expect(t).to.contains('I am a JS Alert')
     })
     // alert window automatically closed by cypress
     cy.get('#result').should('have.text', 'You successfully clicked an alert')
   })
   it('JS confirm Alerts', () => {
     cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
-    cy.get("button[onClick='jsAlert()']").click()
-    cy.on('window.alert', (t) => {
-        expect(t).to.contains('msg in the alert window')
+    cy.get("button[onclick='jsConfirm()']").click()
+    cy.on('window:confirm', (t) => {
+      expect(t).to.contain('I am a JS Confirm')
     })
-    // alert window automatically closed by cypress
-    cy.get('#result').should('have.text', 'You successfully clicked an alert')
+    cy.get('#result').should('have.text', 'You clicked: Ok')
   })
 })
